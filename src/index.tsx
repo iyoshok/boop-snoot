430d076108d6bca24b70fdb3179804e472205d92
iyoshok <iyoshok@protonmail.com>
2022-06-05 23:55:01 +0200

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-import { appWindow } from '@tauri-apps/api/window';
 
 import App from './app';
 
 import './main.css';
-import './fonts.css';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
